90bec97e4a296de3d4627ee10da5701b959a0625
001Rakib <rakibrakibrakib354@gmail.com>
2023-09-11 21:53:47 +0600

    added title author name and image and reading time and hashtags

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,7 +1,40 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
+  const {
+    Title,
+    Cover,
+    Reading_Time,
+    Author_img,
+    Author,
+    Posted_Date,
+    Hashtags,
+  } = blog;
   console.log(blog);
-  return <div></div>;
+  return (
+    <div>
+      <img src={Cover} alt={`Cover Picture of the title ${Title}`} />
+      <div className="flex justify-between">
+        <div className="flex">
+          <img className="w-14" src={Author_img} alt="" />
+          <div className="ml-6">
+            <h3 className="text-2xl"> {Author} </h3>
+            <p>{Posted_Date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{Reading_Time} Min Read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{Title}</h2>
+      <p>
+        {Hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href=""> {hash} </a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 Blog.propTypes = {
   blog: PropTypes.object.isRequired,
